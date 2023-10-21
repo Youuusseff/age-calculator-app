bcd51331cab8b3272299923fa3f169524746e699
Youuusseff <youssefbenomrane45@gmail.com>
2023-10-21 12:17:26 +0100

    final commit

--- main.js
+++ main.js
@@ -1,58 +1,101 @@
 
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-console.log(months)
 const date = new Date();
 let todayDay = date.getDate();
 let todayMonth = date.getMonth();
 let todayYear = date.getFullYear();
-console.log(todayYear);
 let month, day,year;
 function ajouter(){
     let myDay = document.getElementById('day').value;
     let myMonth = document.getElementById('month').value;
     let myYear = document.getElementById('year').value;
-    console.log(myDay)
-    console.log(myMonth);
-    console.log(myYear);
-    let year = todayYear - myYear;
-    console.log(year);
-
-    if (todayMonth < myMonth){
-        year --;  
-        let somme = 365 - months[todayMonth] + todayDay;
-        let i = todayMonth + 1;
-        while( i != (myMonth-1)){
-            somme -= months[i]
-            i ++;
+    let x = myMonth - 1;
+    let inputs = document.getElementsByClassName("inputy");
+        Array.from(inputs).forEach(input =>{
+            input.style.borderColor = "#716f6f6f";
+        })
+        let labels = document.getElementsByTagName("label");
+        Array.from(labels).forEach(label => {
+            label.style.color = "#716F6F";
+        })
+        let errors = document.getElementsByClassName("error")
+        Array.from(errors) .forEach(error => {
+           error.innerText = "";
+        });
+    if((myDay === "")||(myMonth === "") || (myYear == "") || (myDay > months[x]) ||(myDay <=0) || (myMonth > 12) || (myMonth< 1) || (myYear > todayYear) || (myYear <= 0)){
+        document.getElementsByClassName("inputy")[0].style.borderColor = "red";
+        document.getElementsByClassName("inputy")[1].style.borderColor = "red";
+        document.getElementsByClassName("inputy")[2].style.borderColor = "red";
+        document.getElementsByTagName("label")[0].style.color = "red";
+        document.getElementsByTagName("label")[1].style.color = "red";
+        document.getElementsByTagName("label")[2].style.color = "red";
+        if (myDay === ""){
+            document.getElementById("error-day").innerText = "This field is required";
         }
-        somme -= myDay;
-        console.log(somme);
-        month = somme / 30.5;
-        console.log(month);
-        month = parseInt(month);
-        console.log(month);
-        if (todayDay < myDay){
-                day = months[myMonth-1] - (myDay-todayDay);
+        console
+        if (myMonth === ""){
+            document.getElementById("error-month").innerText = "This field is required";
+        }
+        if (myYear === ""){
+            document.getElementById("error-year").innerText = "This field is required";
+        }
+        if ((myDay > 31) || (myDay < 0)||(myDay === 0)){
+            document.getElementById("error-day").innerText = "Must be a valid date";
+        }
+        if ((myMonth > 12) || (myMonth < 0) ||(myMonth === 0)){
+            document.getElementById("error-month").innerText = "Must be a valid date";
         }
         else{
-            day = todayDay - myDay;
+            if (myDay > months[x]){
+                document.getElementById("error-day").innerText = "Must be a valid date";
+            }
         }
-        console.log(day)
+        if ((myYear > todayYear)||(myYear < 1)){
+            document.getElementById("error-year").innerText = "Must be a valid date";
+        }
+        let outYear = document.getElementById("out-year");
+        let outMonth = document.getElementById("out-month");
+        let outDay = document.getElementById("out-day");
+        outYear.innerHTML = "- -";
+        outMonth.innerHTML = "- -";
+        outDay.innerText = "- -";
+        
     }
     else{
-        month = todayMonth +1 - myMonth;
-        if (todayDay < myDay){
-            month --;
-            day = months[myMonth-1] - (myDay-todayDay);
+        let year = todayYear - myYear;
+        if (todayMonth < myMonth){
+            year --;  
+            let somme = 365 - months[todayMonth] + todayDay;
+            let i = todayMonth + 1;
+            while( i != (myMonth-1)){
+                somme -= months[i]
+                i ++;
+            }
+            somme -= myDay;
+            month = somme / 30.5;
+            month = parseInt(month);
+            if (todayDay < myDay){
+                    day = months[myMonth-1] - (myDay-todayDay);
+            }
+            else{
+                day = todayDay - myDay;
+            }
         }
         else{
-            day = todayDay - myDay;
+            month = todayMonth +1 - myMonth;
+            if (todayDay < myDay){
+                month --;
+                day = months[myMonth-1] - (myDay-todayDay);
+            }
+            else{
+                day = todayDay - myDay;
+            }
         }
+        let outYear = document.getElementById("out-year");
+        let outMonth = document.getElementById("out-month");
+        let outDay = document.getElementById("out-day");
+        outYear.innerHTML = year;
+        outMonth.innerHTML = month;
+        outDay.innerText = day;
     }
-    let outYear = document.getElementById("out-year");
-    let outMonth = document.getElementById("out-month");
-    let outDay = document.getElementById("out-day");
-    outYear.innerHTML = year;
-    outMonth.innerHTML = month;
-    outDay.innerText = day;
 }
